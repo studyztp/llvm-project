87180ec09fac0d704aae51e1d282d7226be18a71
studyztp <studyztp@gmail.com>
2024-04-17 22:46:20 +0000

    add counting to each function call

--- llvm/lib/Transforms/Utils/HelloWorld.cpp
+++ llvm/lib/Transforms/Utils/HelloWorld.cpp
@@ -12,6 +12,34 @@ using namespace llvm;
 
 PreservedAnalyses HelloWorldPass::run(Function &F,
                                       FunctionAnalysisManager &AM) {
-  errs() << F.getName() << "\n";
+  LLVMContext &context = F.getContext();
+  auto module = F.getParent();
+  Type* int32Type = Type::getInt32Ty(context);
+  PointerType* int32PtrType = PointerType::get(int32Type, 0);
+  FunctionType *printfType = FunctionType::get(int32Type,
+                                               {int32PtrType}, true);
+  FunctionCallee printfFunction = module->getOrInsertFunction("printf", printfType);
+
+  std::string functionName = F.getName().str();
+  std::string functionCallVarName = functionName + "_callCount";
+
+  GlobalVariable *functionCallCount = module->getGlobalVariable(functionName + functionCallVarName);
+  if (!functionCallCount) {
+      functionCallCount = new GlobalVariable(*module, int32Type, false, GlobalValue::CommonLinkage, 0, functionCallVarName);
+      functionCallCount->setInitializer(0);
+  }
+  
+  Instruction *firstInstruction = &F.front().front();
+  IRBuilder<> builder(firstInstruction);
+
+  Value *loadedCallCount = builder.CreateLoad(int32Type,functionCallCount);
+  Value *addedCallCount = builder.CreateAdd(loadedCallCount, builder.getInt32(1));
+  builder.CreateStore(addedCallCount, functionCallCount);
+
+  std::string printLog = functionName + " called %d times\n";
+  Value *formatStr = builder.CreateGlobalStringPtr(printLog);
+  builder.CreateCall(printfFunction, {formatStr, addedCallCount});
+
+  // errs() << F.getName() << "\n";
   return PreservedAnalyses::all();
 }
