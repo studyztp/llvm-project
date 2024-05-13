8b25370ee3b38bb68ed3f02a0c3125843a07bd54
studyztp <studyztp@gmail.com>
2024-05-13 12:32:32 -0700

    move code out of helloworld

--- llvm/lib/Passes/PassBuilder.cpp
+++ llvm/lib/Passes/PassBuilder.cpp
@@ -355,6 +355,7 @@
 #include "llvm/Transforms/Utils/FixIrreducible.h"
 #include "llvm/Transforms/Utils/HelloWorld.h"
 #include "llvm/Transforms/Utils/IRNormalizer.h"
+#include "llvm/Transforms/Utils/PhaseAnalysis.h"
 #include "llvm/Transforms/Utils/InjectTLIMappings.h"
 #include "llvm/Transforms/Utils/InstructionNamer.h"
 #include "llvm/Transforms/Utils/LibCallsShrinkWrap.h"
