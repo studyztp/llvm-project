fb77b677fb5ca27b0b15123ff583ca080163c6a0
studyztp <studyztp@gmail.com>
2024-05-13 12:37:00 -0700

    restore helloworld pass to default sample

--- llvm/include/llvm/Transforms/Utils/HelloWorld.h
+++ llvm/include/llvm/Transforms/Utils/HelloWorld.h
@@ -10,78 +10,12 @@
 #define LLVM_TRANSFORMS_UTILS_HELLOWORLD_H
 
 #include "llvm/IR/PassManager.h"
-#include "llvm/Support/raw_ostream.h"
-#include "llvm/IR/GlobalVariable.h"
-#include "llvm/IR/IRBuilder.h"
-#include "llvm/IR/InlineAsm.h"
-#include "llvm/Transforms/Instrumentation.h"
-#include "llvm/Transforms/Utils/Cloning.h"
-#include "llvm/Support/FileSystem.h"
-#include <string>
-#include <vector>
-
 
 namespace llvm {
 
-class Module;
-
-struct basicBlockInfo {
-  // function related
-  std::string functionName;
-  uint32_t functionId;
-  bool ifStartOfFunction;
-
-  // basic block related
-  std::string basicBlockName;
-  uint64_t basicBlockCount;
-  uint32_t basicBlockId;
-
-  // pointers to the basic block and function
-  BasicBlock* basicBlock;
-  Function* function;
-};
-
 class HelloWorldPass : public PassInfoMixin<HelloWorldPass> {
-private:
-  uint64_t totalFunctionCount;
-  uint64_t totalBasicBlockCount;
-  uint64_t threshold = 100000000;
-
-  std::vector<basicBlockInfo> basicBlockList;
-
-  bool emptyFunction(Function &F);
-
-  GlobalVariable* createGlobalUint64Array(
-    Module& M,
-    std::string variableName,
-    uint64_t size
-  );
-
-  void modifyROIFunctions(Module &M);
-
-  // temp excluding list
-  std::vector<std::string> exclude_functions = {
-    "print_array",
-    "print_int",
-    "print_hi",
-    "if_reach_threshold",
-    "reset_counter",
-    "increase_counter",
-    "increment_array_element_at",
-    "reset_array_element_at",
-    "increase_array_by",
-    "reset_array",
-    "instrumentationFunction",
-    "write_single_data",
-    "write_array_data",
-    "roi_begin_",
-    "roi_end_",
-  };
-
-  Function* createInstrumentationFunction(Module &M);
-  
 public:
-  PreservedAnalyses run(Module &M, ModuleAnalysisManager &AM);
+  PreservedAnalyses run(Function &F, FunctionAnalysisManager &AM);
 };
 
 } // namespace llvm
