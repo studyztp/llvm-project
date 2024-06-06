83f7af817041b30eb68ac090aa7a49a94a24561a
studyztp <studyztp@gmail.com>
2024-06-05 17:02:58 -0700

    get *function

--- llvm/lib/Transforms/Utils/PhaseBound.cpp
+++ llvm/lib/Transforms/Utils/PhaseBound.cpp
@@ -110,7 +110,7 @@ void PhaseBoundPass::formBasicBlockList(Module& M) {
         basicBlock.functionName = functionName;
         basicBlock.function = function;
 
-        for (auto& block: function->getBasicBlockList()) {
+        for (auto& block: *function) {
             std::regex_search(searchStart, line.cend(), matchBasicBlock, eBasicBlock);
             uint32_t basicBlockId = std::stoi(matchBasicBlock[1]);
             if (block.getName().str() != matchBasicBlock[2]) {
