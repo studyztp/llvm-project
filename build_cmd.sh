d8d56d25dc609138155b0453f0a2cb46dd36731c
studyztp <studyztp@gmail.com>
2024-09-16 11:20:38 -0700

    add compiler-rt library to build option

--- build_cmd.sh
+++ build_cmd.sh
@@ -1,5 +1,5 @@
 cmake -S llvm -B build -G "Unix Makefiles" \
--DLLVM_ENABLE_PROJECTS="clang;flang;openmp;lld" \
+-DLLVM_ENABLE_PROJECTS="clang;flang;openmp;lld;compiler-rt" \
 -DCMAKE_INSTALL_PREFIX=$1 \
 -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD=all
 
